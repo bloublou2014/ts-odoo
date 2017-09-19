6d598a9a861d5fd9f216b6f4c94421e22d3fada6
Bloublou <bloublou2014@gmail.com>
2017-09-19 16:22:35 -0700

    add configuration sample

---
+++ src/tests/Config.sample.ts
@@ -0,0 +1,15 @@
+const config = {
+    database: "yourOdooDatabaseName_should_be_subdomain_name",
+    warehouseCategoryId: 1,
+    companies: [
+        {
+            id: 1,
+            isDefault: true,
+            name: "Your Company",
+            username: "company@email.com",
+            password: "loooooooooooooooooooong_password"
+        }
+    ]
+};
+
+export default config;
